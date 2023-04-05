f1a26f4d36d9a1279523baec6bb14539dbe147e3
Tom Han <tomhyhan@gmail.com>
2023-04-04 22:09:09 -0700

    file

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,14 @@
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  console.log('hello from server');
+  return new Response('Hello, Next.js!');
+}
+
+export async function POST(request: any) {
+  console.log('post from server');
+  // @ts-ignore
+  console.log(request.files);
+  console.log(request.body);
+  // const data = request.files;
+  // console.log(data);
+  return new Response('Hello, Next.js!');
 }
